db4adc87d8228b8069757d99319af72c2a3836dc
vijay <vijaykumar.chinnu5@gmail.com>
2025-07-08 19:26:39 +0530

    remove entries from map

--- src/test/java/collection/removeEntriesfromMap.java
+++ src/test/java/collection/removeEntriesfromMap.java
@@ -1,5 +1,20 @@
 package collection;
 
-public class removeEntriesfromMap {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+public class removeEntriesfromMap {
+	public static void main(String[] args) {
+		Map<Integer,String> map = new LinkedHashMap<>();
+			map.put(101, "abcs");
+			map.put(102, "abcs");
+			map.put(103, "abcs");
+			map.put(105, "abcs");
+			map.put(101, "zxzx");
+			
+		System.out.println(map);
+		
+		map.remove(101);
+		System.out.println(map);
+	}
 }
